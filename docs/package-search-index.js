bb1ea46bd09bd12a513c56f27c683d4a9f2f22e0
CUSTEDLOL <visheshmunoth.vj@gmail.com>
2024-11-14 23:59:14 +0800

    javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"healthcare.main"},{"l":"healthcare.records"},{"l":"healthcare.users"},{"l":"healthcare.users.controllers"},{"l":"healthcare.users.models"},{"l":"healthcare.users.view"}];updateSearchResults();
