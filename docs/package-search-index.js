54686a3d0e90de2325962418d9f26864b9301211
KuroInit <ashwinblgps2013@gmail.com>
2024-11-16 17:10:59 +0800

    new javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"healthcare.main"},{"l":"healthcare.records"},{"l":"healthcare.users"},{"l":"healthcare.users.controllers"},{"l":"healthcare.users.models"},{"l":"healthcare.users.view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"healthcare.main"},{"l":"healthcare.records"},{"l":"healthcare.users.controllers"},{"l":"healthcare.users.models"},{"l":"healthcare.users.view"}];updateSearchResults();
